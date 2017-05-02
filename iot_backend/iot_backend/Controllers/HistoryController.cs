d9c39ff0d0eb94092dd769cd65a1b97e60429540
rickvanosch <rickjlvanosch@gmail.com>
2017-05-02 12:09:31 +0200

    implementation(iot_backend): fixed database+history

---
+++ iot_backend/iot_backend/Controllers/HistoryController.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace iot_backend.Controllers
+{
+    [RoutePrefix("containers/history/{id}")]
+    public class HistoryController : ApiController
+    {
+        private Service service = Service.GetInstance();
+
+        public HistoryController()
+        {
+
+
+        }
+
+
+        /// <summary>
+        /// Returns a list of all the history of a container
+        /// </summary>
+        ///  /// <param name="id">Specify the ID of the container in order to retrieve the history</param>
+        /// <returns>List of Entry, contains various information like dates and filllevels</returns>
+        public List<Entry> Get(string id)
+        {
+            return service.GetHistory(id);
+        }
+
+
+    }
+}
---
+++ iot_backend/iot_backend/Entry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iot_backend
+{
+    public class Entry
+    {
+
+        public Entry()
+        {
+
+        }
+        public Entry(string ID, DateTime dateTime, int fillLevel)
+        {
+            this.ID = ID;
+            this.TimeStamp = dateTime;
+            this.FillLevel = fillLevel;
+        }
+
+        private int fillLevel;
+
+        public int FillLevel
+        {
+            get { return fillLevel; }
+            set { fillLevel = value; }
+        }
+
+        private string id;
+
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private DateTime dateTime;
+
+        public DateTime TimeStamp
+        {
+            get { return dateTime; }
+            set { dateTime = value; }
+        }
+
+    }
+}
